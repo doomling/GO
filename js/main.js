9f9dabfa0c4bbbc130c4918d1252b100302fb8df
belrey <belen@mequilibrium.com>
2018-04-29 21:03:25 -0300

    responsive styles and menu

--- js/main.js
+++ js/main.js
@@ -1,46 +1,42 @@
-//$(document).scroll(function() {
-/*if ($('#box-quote').visible(true)) {
-$('#quote').addClass('fadein');
-*/
-//});
-
-
 $(document).on('load' , onViewport("#quote", "active", 0, function() {
   $('#quote').removeClass('hidden').addClass('fadein');
-
 }));
 
 $(document).on('load' , onViewport("#box-text", "active", 0, function() {
   $('#box-text').removeClass('hidden').addClass('fadein');
-
 }));
 
 $(document).on('load' ,  function() {
   $('#slogan').addClass('fadein');
 });
-/*
-$(document).on('scroll' , onViewport("h3", "active", 0, function() {
-  $('#fifth').scrollTop();
-  $(document).
-}));
-*/
+
 let counter = 1;
 $('#slider').on('click', function() {
-  console.log(counter);
-  //const item = $('#img0');
-  //$('#img0').addClass('hidden');
-  if (counter <= 3) {
+  if (counter <= 5) {
   $('#img' + (counter - 1)).remove();
   $('#slider').append('<img src="img/'+ counter +'.jpg" id="img'+ counter +'">');
   counter++;
 } else {
   $('#img' + (counter - 1)).remove();
   counter = 1;
-    $('#slider').append('<img src="img/avatar_circulo.png" id="img0">');
+    $('#slider').append('<img src="img/0.jpg" id="img0">');
 }
 });
 
-//third party css
+$('#hamburger').on('click', function() {
+  $('#menu-content').removeClass('hidden').addClass('hamburger-content');
+});
+
+$('#close').on('click', function() {
+  $('#menu-content').addClass('hidden').removeClass('hamburger-content');
+});
+
+$('#menu-content li').on('click', function() {
+  console.log('mama');
+  $('#menu-content').addClass('hidden').removeClass('hamburger-content');
+});
+
+// third party code
 
 function onViewport(el, elClass, offset, callback) {
   /*** Based on http://ejohn.org/blog/learning-from-twitter/ ***/
